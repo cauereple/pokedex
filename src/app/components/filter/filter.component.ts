d2fb1528564096f9e8df8a2ec0bcbf7e1e5a270d
Cauê Reple <caue-reple@hotmail.com>
2022-11-16 23:13:17 +0100

    feature: pokemon-filter

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.sass']
 })
@@ -10,6 +10,21 @@ export class FilterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.consoleLog()
   }
+
+  consoleLog() {
+    const selectBtn = document.querySelector(".pokemon-filter__btn")
+    const items = document.querySelectorAll(".pokemon-list__item")
+
+    const menu = document.querySelector(".menu-select")
+
+
+    
+    menu?.addEventListener("click", () => {
+      menu.classList.toggle("open")
+    })
+  }
+  
 
 }
