07495641a09c10ffc22dd8b9b2d2d30d1d51178e
Cauê Reple <caue-reple@hotmail.com>
2022-11-21 23:41:11 +0100

    using angular way to do things instead javascript pure

--- src/app/components/pokemon-header/pokemon-header.component.ts
+++ src/app/components/pokemon-header/pokemon-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-header',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  searchClicked: boolean = false;
+
+  openSearch() {
+    this.searchClicked = !this.searchClicked;
+    this.focoInput()
+  }
+
+  focoInput() {
+    this.renderer.selectRootElement('#search-input').focus();
   }
 
 }
